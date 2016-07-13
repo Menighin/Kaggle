10e344e805e0c24928bc969288b330c2450a08df
Menighin <joao.menighin@gmail.com>
2016-07-12 22:27:56 -0300

    Using probalistic functions + Label Encoder

--- Shelter Animal Outcomes/shelter_animal_outcomes.py
+++ Shelter Animal Outcomes/shelter_animal_outcomes.py
@@ -1,6 +1,7 @@
 # https://www.kaggle.com/c/shelter-animal-outcomes
 import pandas
-from sklearn import linear_model, tree, ensemble
+from sklearn import linear_model, tree, ensemble, preprocessing
+from sklearn.cross_validation import train_test_split, cross_val_score
 import numpy as np
 import re
 import math
@@ -17,12 +18,6 @@ AGE_UPON_OUTCOME = 'AgeuponOutcome'
 BREED = 'Breed'
 COLOR = 'Color'
 
-# Maps
-MAP_COLORS = {'last': 0}
-MAP_BREEDS = {'last': 0}
-MAP_ANIMAL_TYPE = {'Dog': 0, 'Cat': 1}
-MAP_SEX = {'last': 0}
-
 def calculate_age(age):
     try:
         w, s = age.split()
@@ -38,43 +33,28 @@ def calculate_age(age):
     except:
         pass
 
-def map_colors(color):
-    if color not in MAP_COLORS:
-        MAP_COLORS[color] = MAP_COLORS['last']
-        MAP_COLORS['last'] += 1
-    return MAP_COLORS[color]
-
-def map_breeds(breed):
-    if breed not in MAP_BREEDS:
-        MAP_BREEDS[breed] = MAP_BREEDS['last']
-        MAP_BREEDS['last'] += 1
-    return MAP_BREEDS[breed]
-
-def map_animal_type(type):
-    return MAP_ANIMAL_TYPE[type]
-
-def map_sex(sex):
-    if sex not in MAP_SEX:
-        MAP_SEX[sex] = MAP_SEX['last']
-        MAP_SEX['last'] += 1
-    return MAP_SEX[sex]
-
-def clean(data):
-    data[AGE_UPON_OUTCOME] = data[AGE_UPON_OUTCOME].apply(calculate_age) # Age to days
-    data[AGE_UPON_OUTCOME] = data[AGE_UPON_OUTCOME].fillna(data[AGE_UPON_OUTCOME].median()) # Fill empty
-    data[COLOR] = data[COLOR].apply(map_colors) # Map colors to numbers
-    data[BREED] = data[BREED].apply(map_breeds) # Map breeds to numbers
-    data[ANIMAL_TYPE] = data[ANIMAL_TYPE].apply(map_animal_type) # Map animal types
-    data[SEX_UPON_OUTCOME] = data[SEX_UPON_OUTCOME].apply(map_sex) # Map sex
+def clean(train, test):
+    train[AGE_UPON_OUTCOME] = train[AGE_UPON_OUTCOME].apply(calculate_age) # Age to days
+    train[AGE_UPON_OUTCOME] = train[AGE_UPON_OUTCOME].fillna(train[AGE_UPON_OUTCOME].median()) # Fill empty
+    test[AGE_UPON_OUTCOME] = test[AGE_UPON_OUTCOME].apply(calculate_age) # Age to days
+    test[AGE_UPON_OUTCOME] = test[AGE_UPON_OUTCOME].fillna(test[AGE_UPON_OUTCOME].median()) # Fill empty
+    
+    for col in [ANIMAL_TYPE, SEX_UPON_OUTCOME, BREED, COLOR]: # Labeling values
+        train[col] = train[col].fillna('NaN')
+        test[col] = test[col].fillna('NaN')
+        le = preprocessing.LabelEncoder().fit(np.append(train[col], test[col]))
+        train[col] = le.transform(train[col])
+        test[col] = le.transform(test[col])
 
 def main():
     # Base for training
     train = pandas.read_csv('train.csv')
-    clean(train)
 
     # Base for testing
     test = pandas.read_csv('test.csv')
-    clean(test)
+
+    # Cleaning data
+    clean(train, test)
 
     predictors = [AGE_UPON_OUTCOME, COLOR, BREED, ANIMAL_TYPE, SEX_UPON_OUTCOME]
 
@@ -82,22 +62,17 @@ def main():
     #alg = tree.DecisionTreeClassifier()
     alg = ensemble.GradientBoostingClassifier()
 
-    alg.fit(train[predictors], train[OUTCOME_TYPE])
+    fit = alg.fit(train[predictors], train[OUTCOME_TYPE])
 
-    predictions = alg.predict(test[predictors])
+    # Testing with cross validation
+    #print("LogLoss:")
+    #print(cross_val_score(alg, train[predictors], train[OUTCOME_TYPE], cv=3, scoring='log_loss', verbose=0))
 
-    submission = pandas.DataFrame({
-        'ID': test['ID'],
-        'Adoption': predictions == 'Adoption',
-        'Died': predictions == 'Died',
-        'Euthanasia': predictions == 'Euthanasia',
-        'Return_to_owner': predictions == 'Return_to_owner',
-        'Transfer': predictions == 'Transfer'
-    })
+    predictions = alg.predict_proba(test[predictors])
 
-    submission = submission[['ID', 'Adoption', 'Died', 'Euthanasia', 'Return_to_owner', 'Transfer']]
-
-    submission.to_csv("kaggle.csv", header=True, index=False)
+    submission = pandas.DataFrame(predictions, index = test.index, columns = fit.classes_)
+    submission.sort_index(inplace = True)
+    submission.to_csv("kaggle.csv", index_label="ID")   
 
 if __name__ == '__main__':
     main()
