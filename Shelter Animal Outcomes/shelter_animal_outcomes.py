66b8997e2f530b31df962639e379744dcd1d5425
Menighin <joao.menighin@gmail.com>
2016-07-16 12:36:38 -0300

    Datetime features, Sex and Pure Breed

--- Shelter Animal Outcomes/shelter_animal_outcomes.py
+++ Shelter Animal Outcomes/shelter_animal_outcomes.py
@@ -1,5 +1,5 @@
 # https://www.kaggle.com/c/shelter-animal-outcomes
-# Highest by cross: [-0.79007332 -0.78181518 -0.77728684]
+# Highest by cross: [-0.78491288 -0.77744353 -0.77270355]
 import pandas as pd
 from datetime import datetime
 from sklearn import linear_model, tree, ensemble, preprocessing
@@ -30,7 +30,13 @@ COLOR2 = 'Color2'
 HAS_NAME = 'HasName'
 AGE_GROUP = 'AgeGroup'
 WEEK_DAY = 'WeekDay'
+MONTH = 'Month'
+YEAR = 'Year'
 EXIT_HOUR = 'ExitHour'
+IS_OPEN = 'IsOpen'
+IS_PURE_BREED = 'IsPureBreed'
+SEX = 'Sex'
+FERTILE = 'Fertile'
 
 # Global maps
 COLORS_MAP = {'last': 0, 'NaN': -1}
@@ -40,7 +46,7 @@ def calculate_age(age):
         w, s = age.split()
         n = int(w)
         if 'year' in s:
-            return n * 360
+            return n * 365
         elif 'month' in s:
             return n * 30
         elif 'week' in s:
@@ -57,7 +63,7 @@ def process_colors(color):
     for c in color.split('/'):
 
         # Removing colors descriptors
-        c = ' '.join([w for w in c.split(' ') if w not in ['Cream', 'Tabby', 'Brindle', 'Smoke', 'Tiger', 'Merle', 'Tick', 'Point']])
+        c = ' '.join([w for w in c.split(' ') if len(c.split(' ')) > 1 and w not in ['Cream', 'Tabby', 'Brindle', 'Smoke', 'Tiger', 'Merle', 'Tick', 'Point']])
 
         if c not in COLORS_MAP:
             COLORS_MAP[c] = COLORS_MAP['last']
@@ -67,9 +73,6 @@ def process_colors(color):
     if COLOR1 not in cols: cols[COLOR1] = COLORS_MAP['NaN']
     if COLOR2 not in cols: cols[COLOR2] = COLORS_MAP['NaN']
     return pd.Series(cols)
-
-def clean_breed(breed):
-    return ' '.join([w for w in breed.split(' ') if w != 'Mix'])
 
 def clean():
     global train
@@ -86,13 +89,9 @@ def clean():
     test = pd.concat([test, test[COLOR].apply(process_colors)], axis = 1)
     train.drop(COLOR, axis=1, inplace=True)
     test.drop(COLOR, axis=1, inplace=True)
-
-    # Breed
-    train[BREED] = train[BREED].apply(clean_breed)
-    test[BREED] = test[BREED].apply(clean_breed)
     
 
-    for col in [ANIMAL_TYPE, BREED, SEX_UPON_OUTCOME]: # Labeling values
+    for col in [ANIMAL_TYPE, BREED]: # Labeling values
         train[col] = train[col].fillna('NaN')
         test[col] = test[col].fillna('NaN')
         le = preprocessing.LabelEncoder().fit(np.append(train[col], test[col]))
@@ -108,30 +107,27 @@ def add_has_name():
 
 def map_age_group(age):
     g = 0
-    if age <= 30:
+    if age <= 30 * 6: # New Born
         g = 0
-    elif age <= 90:
+    elif age <= 30 * 18: # Junior
         g = 1
-    elif age <= 180:
+    elif age <= 365 * 3: # Prime
         g = 2
-    elif age <= 360:
+    elif age <= 365 * 6: # Mature
         g = 3
-    elif age <= 360 * 5:
+    elif age <= 360 * 10: # Senior
         g = 4
-    else:
+    else: # Geriatric
         g = 5
     return pd.Series({AGE_GROUP: g})
 
 def add_age_group():
-    global test
-    global train
+    global test, train
     train = pd.concat([train, train[AGE_UPON_OUTCOME].apply(map_age_group)], axis = 1)
     test  = pd.concat([test , test [AGE_UPON_OUTCOME].apply(map_age_group)], axis = 1)
 
-
 def add_datetime_features():
-    global test
-    global train
+    global test, train
 
     date_format = '%Y-%m-%d %H:%M:%S'
 
@@ -140,10 +136,47 @@ def add_datetime_features():
     train = pd.concat([train, train[DATE_TIME].apply(week_day)], axis = 1)
     test  = pd.concat([test , test [DATE_TIME].apply(week_day)], axis = 1)
 
+    # Month
+    month = lambda d: pd.Series({MONTH: datetime.strptime(d, date_format).month})
+    train = pd.concat([train, train[DATE_TIME].apply(month)], axis = 1)
+    test  = pd.concat([test , test [DATE_TIME].apply(month)], axis = 1)
+
+    # Year
+    year = lambda d: pd.Series({YEAR: datetime.strptime(d, date_format).year})
+    train = pd.concat([train, train[DATE_TIME].apply(year)], axis = 1)
+    test  = pd.concat([test , test [DATE_TIME].apply(year)], axis = 1)
+
     # ExitHour
     exit_hour = lambda d: pd.Series({EXIT_HOUR: datetime.strptime(d, date_format).hour})
     train = pd.concat([train, train[DATE_TIME].apply(exit_hour)], axis = 1)
     test  = pd.concat([test , test [DATE_TIME].apply(exit_hour)], axis = 1)
+
+    # IsOpen
+    is_open = lambda d: pd.Series({IS_OPEN: 1 if datetime.strptime(d, date_format).hour >= 11 and datetime.strptime(d, date_format).hour <= 19 else 0})
+    train = pd.concat([train, train[DATE_TIME].apply(is_open)], axis = 1)
+    test  = pd.concat([test , test [DATE_TIME].apply(is_open)], axis = 1)
+
+
+def add_is_pure_breed():
+    global test, train
+
+    is_pure = lambda b: pd.Series({IS_PURE_BREED: 1 if 'Mix' not in b else 0})
+    train = pd.concat([train, train[DATE_TIME].apply(is_pure)], axis = 1)
+    test  = pd.concat([test , test [DATE_TIME].apply(is_pure)], axis = 1)
+
+def add_sex_and_fertile():
+    global test, train
+    
+    train[SEX_UPON_OUTCOME] = train[SEX_UPON_OUTCOME].fillna('Unknown')
+    test[SEX_UPON_OUTCOME] = test[SEX_UPON_OUTCOME].fillna('Unknown')
+
+    define_sex = lambda s: pd.Series({SEX: 0 if 'Male' in s else 1})
+    train = pd.concat([train, train[SEX_UPON_OUTCOME].apply(define_sex)], axis = 1)
+    test  = pd.concat([test , test[SEX_UPON_OUTCOME].apply(define_sex)], axis = 1)
+
+    is_fertile = lambda s: pd.Series({FERTILE: 0 if 'Spayed' in s or 'Neutered' in s else 1})
+    train = pd.concat([train, train[SEX_UPON_OUTCOME].apply(is_fertile)], axis = 1)
+    test  = pd.concat([test , test[SEX_UPON_OUTCOME].apply(is_fertile)], axis = 1)
 
 def main():
     global train
@@ -154,6 +187,9 @@ def main():
 
     # Base for testing
     test = pd.read_csv('test.csv')
+
+    # Feature IsPureBreed
+    add_is_pure_breed()
 
     # Cleaning data
     clean()
@@ -167,7 +203,10 @@ def main():
     # Feature WeekDay, ExitHour, WorkingDay and Holiday
     add_datetime_features()
 
-    predictors = [AGE_UPON_OUTCOME, COLOR1, COLOR2, BREED, ANIMAL_TYPE, SEX_UPON_OUTCOME, HAS_NAME, AGE_GROUP, WEEK_DAY, EXIT_HOUR]
+    # Feature of Sex and Fertile
+    add_sex_and_fertile()
+
+    predictors = [AGE_UPON_OUTCOME, COLOR1, COLOR2, BREED, ANIMAL_TYPE, HAS_NAME, AGE_GROUP, WEEK_DAY, MONTH, YEAR, EXIT_HOUR, IS_OPEN, IS_PURE_BREED, SEX, FERTILE]
 
     #alg = linear_model.LogisticRegression(random_state=1)
     #alg = tree.DecisionTreeClassifier()
