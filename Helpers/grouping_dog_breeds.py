63434d38fbac47bec9bc00ae3caeccc2e840ca61
Menighin <joao.menighin@gmail.com>
2016-07-30 00:14:08 -0300

    Grouping Breed Helper - WIP

---
+++ Helpers/grouping_dog_breeds.py
@@ -0,0 +1,17 @@
+'''
+This script is used to crawl dogs.petbreeds.com to search for breeds group by some characteristics as size, temperaments, etc;
+'''
+
+# API URL's (Extracted directly from website)
+
+# BY SIZE -------------------------
+URL_TOY = ''
+URL_SMALL = ''
+URL_MEDIUM = ''
+URL_LARGE = ''
+URL_GIANT = ''
+
+# BY TEMPERAMENT -------------------------
+
+
+# BY SIZE -------------------------
